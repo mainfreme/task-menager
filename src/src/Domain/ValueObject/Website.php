b6610d8a2c2faf1bcb270a78b17cf255fc366f0c
Soft Well House <bialonj@gmail.com>
2026-03-04 21:02:39 +0100

    feat (domain): add Website VO - implement validation using Webmozart Assert - add Doctrine ORM embeddable mapping - ensure immutability of the object

---
+++ src/src/Domain/ValueObject/Website.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\ValueObject;
+
+use Doctrine\ORM\Mapping as ORM;
+use Webmozart\Assert\Assert;
+
+
+#[ORM\Embeddable]
+final class Website extends StringValueObject
+{
+    #[ORM\Column(name: "website", type: "string", length: 255)]
+    protected string $value;
+
+    protected function validate(string $value): void
+    {
+        Assert::notEmpty($value, 'Strona internetowa nie może być pusta');
+        Assert::true(
+            (bool) filter_var($value, FILTER_VALIDATE_URL),
+            'Nieprawidłowy URL: %s',
+            [$value]
+        );
+    }
+
+    public static function fromString(string $value): self
+    {
+        return new self($value);
+    }
+}
