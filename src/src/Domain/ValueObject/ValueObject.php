3ae4d33dd21ec0ca3bbab485aee87f5f12a85d9e
Soft Well House <bialonj@gmail.com>
2026-03-04 19:03:15 +0100

    feat(domain): add Value Object base abstractions - Implement ValueObjectInterface, ValueObject, StringValueObject

---
+++ src/src/Domain/ValueObject/ValueObject.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\ValueObject;
+
+abstract class ValueObject implements ValueObjectInterface
+{
+    public function equals(ValueObjectInterface|self $other): bool
+    {
+        if (!$other instanceof static) {
+            return false;
+        }
+
+        return $this->toComparable() === $other->toComparable();
+    }
+
+    abstract public function toString(): string;
+
+    abstract protected function toComparable(): string|array;
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+}
